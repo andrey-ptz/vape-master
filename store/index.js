755e7f11e2ab1b0c364e3416961d3129542c5e54
Andrey Axenov <agatissimo@gmail.com>
2019-10-22 22:14:39 +0300

    add action for get data from firebase and add mutation for set data to store

--- store/index.js
+++ store/index.js
@@ -1,3 +1,9 @@
+// get data from db and set data to store when app is initializing
 export const actions = {
-
+	async nuxtServerInit({ commit }) {
+		let itemList = [];
+		const atomData = await this.$axios.$get(process.env.baseUrl + '/atomizers.json');
+		for (let key in atomData) itemList.push({ id: key, ...atomData[key] });
+		commit('setItems', { itemList, name: 'atomizers' });
+	}
 }
--- store/mutations.js
+++ store/mutations.js
@@ -1,3 +1,14 @@
-export default {
+// function gets array, sort key and returns sorted array by this sort key
+const sortBySomething = (array, param, reverse = false) => {
+	return array.sort((a, b) => {
+		return a[param] < b[param] ? -1 : (a[param] > b[param] ? 1 : 0)
+	})
+}
 
+export default {
+	// add data from db to store
+	setItems(state, payload) {
+		const itemList = payload.itemList, name = payload.name;
+		state[name].itemList = sortBySomething(itemList, state[name].defaultSort)
+	}
 }
