a764e168b723796f60f53d299b6aa2051e7cf570
Andrey Axenov <agatissimo@gmail.com>
2019-10-23 18:48:30 +0300

    add action and mutation for sort cards

--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,10 @@
 export default {
+	// get query from URL, page name and call mutation
+	// for sort state by orderBy param
 	filterValues({ commit, state }, payload) {
-
+		const query = payload.query, pageState = state[payload.name];
+		('orderBy' in query && pageState.sortParams.some(p => p.value === query.orderBy))
+			? commit('sortItems', { orderBy: query.orderBy, name: payload.name })
+			: commit('sortItems', { orderBy: pageState.defaultSort, name: payload.name })
 	}
 }
--- store/mutations.js
+++ store/mutations.js
@@ -10,5 +10,12 @@ export default {
 	setItems(state, payload) {
 		const itemList = payload.itemList, name = payload.name;
 		state[name].itemList = sortBySomething(itemList, state[name].defaultSort)
+	},
+	// get orderBy param from URL query, page name 
+	// after sort card list and update activeSort param in store
+	sortItems(state, payload) {
+		const name = payload.name, orderBy = payload.orderBy;
+		state[name].itemList = sortBySomething(state[name].itemList, orderBy)
+		state[name].activeSort = orderBy
 	}
 }
