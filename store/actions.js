88d98af04a9fabc471a415bbbeb462a6430972d1
Andrey Axenov <agatissimo@gmail.com>
2019-10-23 19:25:43 +0300

    add mutation that process URL query

--- store/actions.js
+++ store/actions.js
@@ -1,10 +1,13 @@
 export default {
-	// get query from URL, page name and call mutation
+	// get query from URL, get page name and call mutations
 	// for sort state by orderBy param
+	// for set filter params to store
 	filterValues({ commit, state }, payload) {
 		const query = payload.query, pageState = state[payload.name];
 		('orderBy' in query && pageState.sortParams.some(p => p.value === query.orderBy))
 			? commit('sortItems', { orderBy: query.orderBy, name: payload.name })
 			: commit('sortItems', { orderBy: pageState.defaultSort, name: payload.name })
+
+			commit('filterValues', payload)
 	}
 }
--- store/mutations.js
+++ store/mutations.js
@@ -5,6 +5,45 @@ const sortBySomething = (array, param, reverse = false) => {
 	})
 }
 
+// get URL query params, split them and replace string values 
+const replaceQueryValue = (queryValues, stateValues) => {
+	const replacedQuery = {};
+	Object.keys(queryValues).forEach((key) => {
+		const valArr = [];
+		if (queryValues[key].length && key !== 'orderBy') {
+			queryValues[key].split('-').forEach((v) => {
+				const value = v.toLowerCase().trim();
+
+				if (value !== '') {
+					if (!isNaN(value)) valArr.push(parseFloat(value))
+					else if (value === 'true') valArr.push(true)
+					else if (value === 'false') valArr.push(false)
+					else {
+						stateValues.forEach((param, i) => {
+							if (key === param.name) {
+								param.values.forEach(p => {
+									if (p.toLowerCase() === value) valArr.push(p);
+								})
+							}
+						})
+					}
+				}
+
+			})
+			replacedQuery[key] = valArr
+		}
+		else if (key !== 'orderBy') replacedQuery[key] = []
+	})
+	return replacedQuery
+}
+
+// check that filter params array from store include params from URL query
+const compareArrays = (paramArray, queryArray, filterType) => {
+	return filterType === 'checkbox'
+		? queryArray.every(item => paramArray.includes(item))
+		: queryArray[0] >= paramArray[0] && queryArray[1] <= paramArray[1]
+}
+
 export default {
 	// add data from db to store
 	setItems(state, payload) {
@@ -17,5 +56,27 @@ export default {
 		const name = payload.name, orderBy = payload.orderBy;
 		state[name].itemList = sortBySomething(state[name].itemList, orderBy)
 		state[name].activeSort = orderBy
+	},
+	// get URL query params, process them and set to store
+	filterValues(state, payload) {
+		const query = payload.query, name = payload.name;
+		const replacedQuery = replaceQueryValue(query, state[name].filterParams);
+		if (Object.keys(replacedQuery).length) {
+			state[name].filterParams.forEach((param, i) => {
+
+				Object.keys(replacedQuery).forEach((key) => {
+					if (param.name === key) {
+						compareArrays(param.values, replacedQuery[key], param.type)
+							? state[name].filterParams[i].activeValues = replacedQuery[key]
+							: ''
+					}
+				})
+
+			})
+		} else {
+			state[name].filterParams.forEach((param, i) => {
+				state[name].filterParams[i].activeValues = []
+			})
+		}
 	}
 }
